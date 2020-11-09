29151b41b80d0335e5e064b421e5d6ca4798fe20
Tamanna Verma <tamannav01@gmail.com>
2020-11-09 07:43:14 +0530

    Update menu1 (1).py

--- menu1 (1).py
+++ menu1 (1).py
@@ -3,19 +3,19 @@ from requests import get
 
 
 def Hadoop():
-	 print("Setting up Hadoop on your LinuxOS\n 1. Installing jdk")
-    os.system('yum install jdk-8u171-linux-x64.rpm')
-    print("2. Installing Hadoop version-1")
-    os.system('yum install hadoop-1.2.1-1.x86_64.rpm  --force')
-    print('Hadoop setup complete')
+     print("Setting up Hadoop on your LinuxOS\n 1. Installing jdk")
+     os.system('yum install jdk-8u171-linux-x64.rpm')
+     print("2. Installing Hadoop version-1")
+     os.system('yum install hadoop-1.2.1-1.x86_64.rpm  --force')
+     print('Hadoop setup complete')
 
-    print("""1. Configure Hadoop as a namenode
-    2. Configure Hadoop as datanode
-    3. Configure Hadoop as a client node""")
+     print("""1. Configure Hadoop as a namenode
+     2. Configure Hadoop as datanode
+     3. Configure Hadoop as a client node""")
 
-    i = int(input("enter your choice:"))
+     i = int(input("enter your choice:"))
 
-    if i == 1:
+     if i == 1:
         print(" Configuring Hadoop as a namenode")
 		# getting public through external api in json format
         ip=json.loads(get('https://api.ipify.org?format=json').text)
@@ -28,11 +28,12 @@ def Hadoop():
         with open('/etc/hadoop/hdfs-site.xml', 'w+') as file:
             str = "<property>\n<name>dfs.name.dir</name>\n<value>/nn1</value>\n</property>\n"
             file.write(str)
-
+			
+		os.system("hadoop namenode -format")
         os.system('hadoop-daemon.sh  start namenode')
         
 
-    if i == 2:
+     if i == 2:
         print(" Configuring Hadoop as a datanode")
         ip = str(input(" Enter Master's IP:"))
         with open('/etc/hadoop/core-site.xml', 'w+') as file:
@@ -45,7 +46,7 @@ def Hadoop():
 
         os.system('hadoop-daemon.sh  start datanode')
 
-    if i == 3:
+     if i == 3:
         print(" Configuring Hadoop as a client node")
         ip = str(input(" Enter Master's IP:"))
         with open('/etc/hadoop/core-site.xml', 'w+') as file:
@@ -232,58 +233,116 @@ def Aws():
 		os.system("describe-instances")
 	
 	elif i == 5:
-		groupname=input("Enter the name of security group:  ")
-         	groupdescription=input("Enter the description you want to give to your security group :  ")
-         	os.system("aws ec2 create-security-group --group-name {} --description {}".format(groupname,groupdescription))
+            groupname=input("Enter the name of security group:  ")
+            groupdescription=input("Enter the description you want to give to your security group :  ")
+            os.system("aws ec2 create-security-group --group-name {} --description {}".format(groupname,groupdescription))
 	
 	elif i == 6:
-		imageid=input("Enter the image id: ")
-         	instance_type=input("Enter the instance type you want to have: ")
-         	instance_number=input("Enter the number of instances you want to create: ")
-         	subnet_id=input("Enter the subnet id: ")
-         	security_group=input("Enter the ids of security groups you want to provide: ")
-         	keyname=input("Enter the key pair you want to give: ")
-         	os.system("aws ec2 run-instances --image-id {} --instance-type {} --count {} --subnet-id {} --security-group-ids {} --key-name {}".format(imageid,instance_type,instance_number,subnet_id,security_group,keyname))
+            imageid=input("Enter the image id: ")
+            instance_type=input("Enter the instance type you want to have: ")
+            instance_number=input("Enter the number of instances you want to create: ")
+            subnet_id=input("Enter the subnet id: ")
+            security_group=input("Enter the ids of security groups you want to provide: ")
+            keyname=input("Enter the key pair you want to give: ")
+            os.system("aws ec2 run-instances --image-id {} --instance-type {} --count {} --subnet-id {} --security-group-ids {} --key-name {}".format(imageid,instance_type,instance_number,subnet_id,security_group,keyname))
 	
 	elif i ==7:
-		avail_zone=input("Enter the availability zone you want to launch the ebs volume in")
-         	size=input("Enter the size of ebs volume")
-         	os.system("aws ec2 create-volume --availability-zone {} --size {} --volume-type gp2".format(avail_zone,size))
+            avail_zone=input("Enter the availability zone you want to launch the ebs volume in")
+            size=input("Enter the size of ebs volume")
+            os.system("aws ec2 create-volume --availability-zone {} --size {} --volume-type gp2".format(avail_zone,size))
 		
 
 	elif i ==8:
-		instance_id=input("Enter the id of instance to which you want to attach an ebs volume:  ")
-         	volume_id=input("Enter the id of the volume that you want to attach to the instance:  ")
-         	os.system("aws ec2 attach-volume --volume-id {} --instance-id {} --device /dev/sdf".format(volume_id,instance_id))
+            instance_id=input("Enter the id of instance to which you want to attach an ebs volume:  ")
+            volume_id=input("Enter the id of the volume that you want to attach to the instance:  ")
+            os.system("aws ec2 attach-volume --volume-id {} --instance-id {} --device /dev/sdf".format(volume_id,instance_id))
 	
 	elif i == 9:
-		volume_id=input("Enter the id of the volume that you want to detach:  ")
-         	os.system("aws ec2 detach-volume --volume-id {}".format(volume_id))
+            volume_id=input("Enter the id of the volume that you want to detach:  ")
+            os.system("aws ec2 detach-volume --volume-id {}".format(volume_id))
 
 	elif i == 10:
-		name=input("Enter the name you want to give to the new s3 bucket(it should be unique):")
-         	os.system("aws s3 mb s3://{}".format(name))
+	    name=input("Enter the name you want to give to the new s3 bucket(it should be unique):")
+            os.system("aws s3 mb s3://{}".format(name))
 	
 	elif i == 11:
-		bucketname=input("Enter the s3 bucket name")
-         	os.system("aws cloudfront create-distribution --origin-domain-name {}.s3.amazonaws.com".format(bucketname))
+            bucketname=input("Enter the s3 bucket name")
+            os.system("aws cloudfront create-distribution --origin-domain-name {}.s3.amazonaws.com".format(bucketname))
 	
 	elif i == 12:
-		instance_id=input("Enter the id of instance which you want to start:  ")
-         	os.system("aws ec2 start-instances --instance-ids {}".format(instance_id))
+	    instance_id=input("Enter the id of instance which you want to start:  ")
+            os.system("aws ec2 start-instances --instance-ids {}".format(instance_id))
 	
 	elif i == 13:
-		instance_id=input("Enter the id of instance which you want to stop:  ")
-         	os.system("aws ec2 stop-instances --instance-ids {}".format(instance_id))
+            instance_id=input("Enter the id of instance which you want to stop:  ")
+            os.system("aws ec2 stop-instances --instance-ids {}".format(instance_id))
 	
 	elif i ==14:
-		instance_id=input("Enter the id of instance which you want to terminate:  ")
-         	os.system("aws ec2 terminate-instances --instance-ids {}".format(instance_id))
+	    instance_id=input("Enter the id of instance which you want to terminate:  ")
+            os.system("aws ec2 terminate-instances --instance-ids {}".format(instance_id))
 	
 	else:
-		print("you have selected",i,"this is not in option")
-		print("invalid selection!")		
+            print("you have selected",i,"this is not in option")
+            print("invalid selection!")		
 	
+        
+def Partitions():
+    print("""
+    \n
+    Press 1 : To show all partitions
+    Press 2 : To create a physical volume
+    Press 3 : To display all the physical volume
+    Press 4 : To create a volume group
+    Press 5 : To display all the volume groups
+    Press 6 : To create a lv partition
+    Press 7 : To display all the lv partitions
+    Press 8 : To format the partition
+    Press 9 : To mount the partition
+    Press 10 : To resize a lv partition
+    Press 11 : To format only the resized partition
+    """)
+    ch = input("Enter your choice:")
+
+    if int(ch)==1:
+	os.system("fdisk -l")
+    elif int(ch)==2:
+	disk1= input("Enter disk name:")
+	disk2= input("Enter disk name:")
+	os.system("pvcreate {} {}".format(disk1,disk2))
+    elif int(ch)==3:
+	os.system("pvdisplay")
+    elif int(ch)==4:
+	vg= input("Enter volume group name:")
+	diskvg1= input("Enter disk to create vg with:")
+	diskvg2= input("Enter disk to create vg with:")
+	os.system("vgcreate {} {} {}".format(vg,diskvg1,diskvg2))
+    elif int(ch)==5:
+	os.system("vgdisplay")
+    elif int(ch)==6:
+	lv= input("Enter vg name:")
+	size= input("Enter size of the lv to create:")
+	name= input("Enter name of the lv:")
+	os.system("lvcreate --size {} --name {} {}".format(size,name,lv))
+    elif int(ch)==7:
+	os.system("lvdisplay")
+    elif int(ch)==8:
+	dir= input("Enter the location to be formatted:")
+	os.system("mkfs.ext4 {}".format(dir))
+    elif int(ch)==9:
+	mount= input("Enter a partition to mount:")
+	node= input("Enter a dir to mount:")
+	os.system("mount {} {}".format(mount,node))
+    elif int(ch)==10:
+	um= input("Enter partition to unmount:")
+	dis= input("Enter disk name to resize again:")
+	os.system("umount {}".format(um))
+	os.system("fdisk {}".format(dis))
+    elif int(ch)==11:
+	di= input("Enter partition to examine filesystems and format:")
+	os.system("e2fsck -f {}".format(di))
+	os.system("resize2fs {}".format(di))
+    else:
+	print("Enter valid number")
 
 
 
@@ -303,7 +362,7 @@ os.system("tput setaf 1")
 print("			Welcome to my Menu !!")
 os.system("tput setaf 2")
 print("-----------------------------------------------------")
-print("1.Docker\n2.Web server\n3.Hadoop\n4.Linux command")
+print("1.Docker\n2.Web server\n3.Hadoop\n4.Linux command\n5.Partitions")
 i = int(input("Enter your choice where do you want to perform task:"))
 if i == 1:
 	Docker()
@@ -313,19 +372,10 @@ elif i == 3:
 	Aws()
 elif i == 4:
 	Hadoop()
+elif i == 5:
+        Partitions()
 else:
 	print("you have selected",i,"this is not in option")
 	print("invalid selection!")		
 
-	
-	
- 
-	
-	
-
-
-
-
-
-		
 	
